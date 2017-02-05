d0ab1e7a265aea8a93182678ee2d2e4f06900a9c
Angular CLI <angular-cli@angular.io>
2017-02-05 10:17:38 +0100

    chore: initial commit from @angular/cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { JlmPBPage } from './app.po';
+
+describe('jlm-pb App', function() {
+  let page: JlmPBPage;
+
+  beforeEach(() => {
+    page = new JlmPBPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
