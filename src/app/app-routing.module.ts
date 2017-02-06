426628c0b268f9ad2a0a3d28e473d90d0055780c
Julien Teyteau <iuliust@gmail.com>
2017-02-06 04:08:28 +0100

    feat(routes) add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './views/home/home.component';
+
 const routes: Routes = [
   {
     path: '',
-    children: []
+    children: [
+      { path: '', component: HomeComponent }
+    ]
   }
 ];
 
