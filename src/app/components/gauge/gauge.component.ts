96af9bf18a827057995e25692d936e4cae038ccb
Julien Teyteau <iuliust@gmail.com>
2017-02-09 22:55:10 +0100

    feat(gauge) init gauge component

---
+++ src/app/components/gauge/gauge.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jlm-gauge',
+  templateUrl: './gauge.component.html',
+  styleUrls: ['./gauge.component.scss']
+})
+export class GaugeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
