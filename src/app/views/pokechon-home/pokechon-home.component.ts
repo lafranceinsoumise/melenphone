8b7659b3043d011e91d0222961a9a28c196cb2bb
Julien Teyteau <iuliust@gmail.com>
2017-02-09 22:15:15 +0100

    feat(pokechon) create pokechon component

---
+++ src/app/views/pokechon-home/pokechon-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jlm-pokechon-home',
+  templateUrl: './pokechon-home.component.html',
+  styleUrls: ['./pokechon-home.component.scss']
+})
+export class PokechonHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
