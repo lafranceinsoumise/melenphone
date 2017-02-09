ec59348a7d7c117058ae98f029eaf24ddf03842c
Julien Teyteau <iuliust@gmail.com>
2017-02-09 23:41:00 +0100

    init(overlay)

---
+++ src/app/shared/overlay.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jlm-overlay',
+    template: `
+    <div></div>
+    `
+})
+export class OverlayComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { OverlayComponent } from './overlay.component';
+
+@NgModule({
+    imports: [],
+    exports: [
+        OverlayComponent
+    ],
+    declarations: [
+        OverlayComponent
+    ]
+})
+export class SharedModule {}
