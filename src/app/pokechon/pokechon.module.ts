e748b802b6f3ab3c371d5fb628874287828c58f4
Julien Teyteau <iuliust@gmail.com>
2017-02-10 21:27:36 +0100

    init(pokechonWelcome)

--- src/app/pokechon/pokechon.module.ts
+++ src/app/pokechon/pokechon.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CodexComponent } from './views/codex/codex.component';
+import { WelcomeComponent } from './views/welcome/welcome.component';
 
 const routes: Routes = [
     { path: '', component: CodexComponent }
@@ -11,7 +12,7 @@ const routes: Routes = [
     imports: [
         RouterModule.forChild(routes)
     ],
-    declarations: [CodexComponent],
+    declarations: [CodexComponent, WelcomeComponent],
     providers: [],
     entryComponents: []
 })
