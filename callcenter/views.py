aafe1afab60900bd4f3ab1102cddb6772dda3b9f
Luc Freyermuth <luc.freyermuth@eisti.fr>
2017-02-13 19:38:41 +0100

    Code commenté et segmenté

--- callcenter/views.py
+++ callcenter/views.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
+
+#Django imports
 from django.shortcuts import render, redirect
 from django.db import models
-from callcenter.models import *
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-import requests
 from django.conf import settings
-import json
-from .forms import *
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-import numpy as np
+
+#Python imports
+import requests
+import json
+
+#Project imports
+from callcenter.models import *
+from callcenter.map import *
+from .forms import *
+
 
 @require_POST
-@csrf_exempt
+@csrf_exempt #Sinon la requete est bloquée car lancée depuis un autre site (donc qui n'a pas le cookie csrf)
 def noteWebhook(request):
     jsondata = request.body
     data = json.loads(jsondata)
@@ -28,6 +35,8 @@ def noteWebhook(request):
 def index(request):
     return render(request, 'callcenter/index.html', locals())
 
+
+# View pour l'enregistrement d'un nouveau membre et création de son agent callhub
 def registerNew(request):
     if request.method == 'POST':
         form = registerNewForm(request.POST)
@@ -40,14 +49,13 @@ def registerNew(request):
             token = 'Token ' + settings.CALLHUB_API_KHEY
             headers = {'Authorization': token }
             data = {'username':username, 'email':email, 'team':'tout_le_monde'}
-            r = requests.post('https://api.callhub.io/v1/agents/', data=data, headers=headers)
-            if r.status_code == requests.codes.created: #Tout s'est bien passé
-
+            r = requests.post('https://api.callhub.io/v1/agents/', data=data, headers=headers) #On fait la requete sur l'API de github
+            if r.status_code == requests.codes.created: #Si tout s'est bien passé
                 #On recup la localisation via l'API Google maps.
                 adress = city + '+' + country
                 adress.replace(" ", "+")
                 url = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ adress + '&key=' + settings.GOOGLE_MAPS_API_KHEY
-                googleAPIRequest = requests.get(url)
+                googleAPIRequest = requests.get(url) #On fait la requete sur l'API de Google maps
                 if googleAPIRequest.status_code == 200: #Si on a une réponse de google
                     googleAPIData = json.loads(googleAPIRequest.text)
                     if googleAPIData['status'] == "OK": #Si google a un resultat, on le récupere
@@ -62,15 +70,16 @@ def registerNew(request):
                     location_lat="None"
                     location_long="None"
                 #On crée le user dans la bdd
-                user,created = User.objects.get_or_create(username=username, email=email)
+                user,created = User.objects.get_or_create(username=username, email=email) #On tente de créer le user
                 if created:
                     user.set_password(password)
                     user.save()
+                    #On crée alors la partie userextend
                     userExtend, created = UserExtend.objects.get_or_create(agentUsername=username, phi=0, location_lat=location_lat, location_long=location_long, user=user)
                     if created:
                         userExtend.save()
                         return redirect('register_sucess')
-                    else:
+                    else: #Si y'a un problème, on supprime le User créé juste avant
                         user.delete()
                         form.add_error(None, "Une erreur est survenue.")
                 else:
@@ -83,9 +92,9 @@ def registerNew(request):
         form=registerNewForm()
     return render(request, 'callcenter/register.html', locals())
 
+#View de transition après enregistrement d'un nouvel utilisateur
 def registerSucess(request):
     return render(request, 'callcenter/register_success.html', locals())
-
 
 #View pour faire les tests
 def test(request):
@@ -98,44 +107,3 @@ def test(request):
     (X,Y) = getSVGPos(location_lat, location_long)
     test = "X : " + str(X) + " Y : " + str(Y)
     return render(request, 'callcenter/test.html', locals())
-
-
-def getSVGPos(lat,lng):
-    #Latitude : X (augmente vers le nord)
-    #Longitude : Y (augmente vers l'est)
-    # Format sous tableaux : [lat_nord / Ymin, lat_sud / Ymax ,lng_ouest / Xmin ,lng_est / Xmax ]
-
-    SVGData = [[0.001123,0.701235,0.177445,0.687332], #Region 0 FRANCE METROPOLITAINE
-                    ]
-    AreaData = [[51.088954,42.333188,-4.796524,8.203037], #Region 0 FRANCE METROPOLITAINE
-                    ]
-
-    region = findCaseSVG(AreaData, lat, lng) # On cherche dans quelle region on est
-
-    #Calcul de la position sur le SVG :
-
-    #Transformation en radians de la lat :
-    AreaData[region][0] = AreaData[region][0]*np.pi/180
-    AreaData[region][1] = AreaData[region][1]*np.pi/180
-    lat = lat*np.pi/180
-    
-    #Projection de mercator
-    AreaData[region][0] = np.log(np.tan(AreaData[region][0]) + (1/np.cos(AreaData[region][0])))
-    AreaData[region][1] = np.log(np.tan(AreaData[region][1]) + (1/np.cos(AreaData[region][1])))
-    lat = np.log(np.tan(lat) + (1/np.cos(lat)))
-
-    # Y = Ymin + ((lat_nord - lat)/(lat_nord - lat_sud))*(Ymax-Ymin)
-    YSVG = SVGData[region][0] + (((AreaData[region][0] - lat) / (AreaData[region][0] - AreaData[region][1])) * (SVGData[region][1] - SVGData[region][0]))
-
-    # X = Xmin + ((lng - lng_ouest)/(lng_est - lng_ouest))*(Xmax-Xmin)
-    XSVG = SVGData[region][2] + (((lng - AreaData[region][2]) / (AreaData[region][3] - AreaData[region][2])) * (SVGData[region][3] - SVGData[region][2]))
-
-    return(XSVG, YSVG)
-
-def findCaseSVG(AreaData, lat, lng):
-
-    for i in range (len(AreaData)):
-        if ((AreaData[i][0] > lat) and (AreaData[i][1] < lat) and (AreaData[i][2] < lng) and (AreaData[i][3]) > lng):
-            return (i)
-
-    return (len(AreaData))
