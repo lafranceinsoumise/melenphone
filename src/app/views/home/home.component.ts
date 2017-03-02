1f07cb97685c3979a4059921fd7585669a0d4d06
Julien Teyteau <iuliust@gmail.com>
2017-03-02 04:11:38 +0100

    feat(ws) add button that triggers ws endpoint

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { CoordinatesConverterService, SocketConnectionService } from '../../shared';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'jlm-home',
@@ -9,7 +11,10 @@ import { CoordinatesConverterService, SocketConnectionService } from '../../shar
 export class HomeComponent implements OnInit {
   lilDucks: {component: any, inputs: Object}[] = [];
 
-  constructor(private coordsConverter: CoordinatesConverterService, private wsc: SocketConnectionService) { }
+  constructor(
+    private coordsConverter: CoordinatesConverterService,
+    private wsc: SocketConnectionService,
+    private http: Http) { }
 
   ngOnInit() {
     console.log(this.coordsConverter.getSvgLocation(48.577725, 7.738060));
@@ -29,6 +34,24 @@ export class HomeComponent implements OnInit {
     this.wsc.room.addEventListener('close', (event) => {
       console.log(event);
     });
+  }
+
+  makeBackendRequest() {
+    const sentObject = {
+      name: 'Robert',
+      lastName: 'Baratheon',
+      gps: {
+        lat: '55',
+        lng: '40'
+      }
+    };
+    this.http.post('/api/test_websocket/', sentObject)
+      .toPromise()
+      .then((res: Response) => {
+        console.log('[POST] api/test_websocket/ ', res.status);
+        res.json();
+      })
+      .catch(error => console.error(error));
   }
 
 }
