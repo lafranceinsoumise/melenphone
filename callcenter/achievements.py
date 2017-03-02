8499de42873f5b010ca63eeb4887f726a07815b3
Luc Freyermuth <luc.freyermuth@eisti.fr>
2017-03-02 16:07:59 +0100

    Début des achievements

--- callcenter/achievements.py
+++ callcenter/achievements.py
@@ -6,9 +6,18 @@
 from callcenter.models import *
 
 #Python imports
+import datetime
 
 #Fonction principale
 def updateAchievements(agentUsername):
-    if UserExtend.objects.filter(agentUsername=username).exists(): #On check si le user existe
-        user = UserExtend.objects.filter(agentUsername=username)[0].user
+    if UserExtend.objects.filter(agentUsername=agentUsername).exists(): #On check si le user existe
+        user = UserExtend.objects.filter(agentUsername=agentUsername)[0].user
         #Appeler les autres fonctions
+        functions = [leet]
+        for f in functions:
+            f(user)
+
+def leet(user):
+    lastCall = Appel.objects.filter(user=user).order_by('-date')[0].date
+    if date.minute == 37 and date.hour == 13:
+        pass #Unlock l'achievement leet
