2bf1f080e483736a99cd8bb3287639d974cc228b
Luc Freyermuth <luc.freyermuth@eisti.fr>
2017-03-06 22:52:09 +0100

    basic achievements

--- callcenter/achievements.py
+++ callcenter/achievements.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #Django imports
-
+from django.utils import timezone
 #Project import
 from callcenter.models import *
 
@@ -38,11 +38,53 @@ def leet(user, calls):
     if lastCall.minute == 37 and lastCall.hour == 13:
         unlockAchievement("leet", user)
 
+def dailycalls(user, calls):
+    dailyCalls = calls.filter(date__gte=timezone.now() - datetime.timedelta(days=9)).count
+    if dailyCalls == 30:
+        unlockAchievement("daily_a_fond", user)
+    if dailyCalls == 50:
+        unlockAchievement("daily_acharne", user)
+    if dailyCalls == 100:
+        unlockAchievement("daily_dodo", user)
+
+
 def callCount(user, calls):
     count = calls.count
     if count == 1:
-        unlockAchievement("count_insoumis", user)
+        unlockAchievement("count_insoumis_1", user)
+    if count == 5:
+        unlockAchievement("count_insoumis_2", user)
     if count == 10:
-        unlockAchievement("count_insoumis_qualite", user)
+        unlockAchievement("count_insoumis_3", user)
+    if count == 20:
+        unlockAchievement("count_insoumis_4", user)
+    if count == 35:
+        unlockAchievement("count_insoumis_5", user)
     if count == 50:
-        unlockAchievement("count_conseiller_issou", user)
+        unlockAchievement("count_membre_appui", user)
+    if count == 70:
+        unlockAchievement("count_createur_appui", user)
+    if count == 100:
+        unlockAchievement("count_conseiller_suppleant", user)
+    if count == 150:
+        unlockAchievement("count_conseiller_titulaire", user)
+    if count == 250:
+        unlockAchievement("count_adjoint", user)
+    if count == 375:
+        unlockAchievement("count_maire", user)
+    if count == 500:
+        unlockAchievement("count_directeur", user)
+    if count == 700:
+        unlockAchievement("count_conseiller_departemental", user)
+    if count == 1000:
+        unlockAchievement("count_president_departemental", user)
+    if count == 1500:
+        unlockAchievement("count_conseiller_regional", user)
+    if count == 2000:
+        unlockAchievement("count_president_regional", user)
+    if count == 2750:
+        unlockAchievement("count_depute_5", user)
+    if count == 3750:
+        unlockAchievement("count_depute_constituante", user)
+    if count == 5000:
+        unlockAchievement("count_depute_6", user)
