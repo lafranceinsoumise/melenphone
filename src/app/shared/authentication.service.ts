e056f2e1109ed1f39373c2189a4d38c490d95f83
Julien Teyteau <iuliust@gmail.com>
2017-03-09 02:27:27 +0100

    fix(login) auth works

--- src/app/shared/authentication.service.ts
+++ src/app/shared/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
@@ -13,7 +13,10 @@ export class AuthenticationService {
     }
 
     login(username: string, password: string) {
-        return this.http.post('/api/token/auth', `username=${username}&password=${password}`)
+        const headers = new Headers({
+            'Content-Type': 'application/json'
+        })
+        return this.http.post('/api/token/auth', {username, password})
             .toPromise()
             .then((res: Response) => {
                 const jsonObj = res.json();
