c46fef822a202323d7f1076d07b6534410fb43d3
Luc Freyermuth <luc.freyermuth@eisti.fr>
2017-03-11 23:26:16 +0100

    Serializer validation

--- callcenter/serializers.py
+++ callcenter/serializers.py
@@ -1,7 +1,8 @@
-from rest_framework.serializers import ModelSerializer, RelatedField
+from rest_framework.serializers import ModelSerializer, RelatedField, ValidationError
 
 from .models import UserExtend
 from accounts.models import User
+from django.conf import settings
 
 class UserSerializer(ModelSerializer):
     class Meta:
@@ -23,6 +24,7 @@ class UserExtendSerializer(ModelSerializer):
         )
 
     def update(self, instance, validated_data):
+        print(instance.pk)
         instance.agentUsername = validated_data.get('agentUsername', instance.agentUsername)
         instance.save()
 
@@ -32,5 +34,24 @@ class UserExtendSerializer(ModelSerializer):
             user.city = location.get('city', user.city)
             user.country_code = location.get('country_code', user.country_code)
             user.save()
+
+        if instance.pk is None: #On cree le user
+            print('lol')
+            token = 'Token ' + settings.CALLHUB_API_KHEY
+            headers = {'Authorization': token }
+            callhubData = {'username':validated_data.get('agentUsername', instance.agentUsername), 'email':user.email, 'team':'tout_le_monde'}
+
+            r = requests.post('https://api.callhub.io/v1/agents/', data=callhubData, headers=headers) #On fait la requete sur l'API de github
+            if r.status_code == requests.codes.created: #Si tout s'est bien passé
+                #On enregistre le UserExtend dans la bdd
+                    instance.agentUsername = validated_data.get('agentUsername', instance.agentUsername)
+                    instance.save()
+
+            elif r.status_code == 400: #Bad request : le username existe déjà !
+                raise ValidationError("Un agent Callhub porte déjà ce nom.")
+
+            else: #Autre erreur de callhub
+                raise ValidationError("Callhub ne répond pas. Réessayez plus tard !")
+
 
         return instance
