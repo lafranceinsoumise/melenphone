0b7fc2ef51a489830058996b29700ea33e72041c
Julien Teyteau <iuliust@gmail.com>
2017-03-12 02:01:32 +0100

    fix(user) update user model

--- src/app/shared/_models/user.model.ts
+++ src/app/shared/_models/user.model.ts
@@ -1,8 +1,14 @@
 export class User {
-    username: string;
-    password: string;
-    email: string;
-    phi: string;
-    phi_multiplier: number;
-    leaederboard: any;
+  id: number;
+  agentUsername: string;
+  email: string;
+  location: {
+      city: string;
+      country_code: string;
+  };
+  phi: number;
+  phi_multiplier: number;
+  alltime_leaderboard: number;
+  weekly_leaderboard: number;
+  daily_leaderboard: number;
 }
