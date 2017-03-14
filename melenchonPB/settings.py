aea84ba3ef489baa4a5174baf2f7a7190063fcb0
Luc Freyermuth <luc.freyermuth@eisti.fr>
2017-03-14 21:25:06 +0100

    Now serving ngApp/dist/index.html

--- melenchonPB/settings.py
+++ melenchonPB/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'melenchonPB.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'callcenter/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'ngApp/dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
