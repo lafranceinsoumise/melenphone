79bba5c101a487555728130d04b6aed05c1b6f5b
Julien Teyteau <iuliust@gmail.com>
2017-03-27 03:24:20 +0200

    style(oauth) change form messages

---
+++ src/app/views/apropos/apropos.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jlm-apropos',
+  templateUrl: './apropos.component.html',
+  styleUrls: ['./apropos.component.scss']
+})
+export class AproposComponent {}
