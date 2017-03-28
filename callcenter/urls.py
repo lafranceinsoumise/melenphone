a0b7c68cb2b89992b18dacdab056229af8aafd2b
Luc Freyermuth <luc.freyermuth@eisti.fr>
2017-03-28 02:14:36 +0200

    Test urls are now only included if DEBUG=True

--- callcenter/urls.py
+++ callcenter/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     # API - NO AUTH REQUIRED
-    url(r'^test_websocket$', views.api_test_socket.as_view()),
-    url(r'^simulate_call$', views.api_test_simulatecall.as_view()),
-    url(r'^simulate_achievement$', views.api_test_simulateachievement.as_view()),
     url(r'^basic_information$', views.api_basic_information.as_view()),
     url(r'^leaderboard$', views.api_leaderboard.as_view()),
     # API - AUTH REQUIRED
@@ -15,3 +13,10 @@ urlpatterns = [
     url(r'^current_user/associate_existing_agent$', views.AssociateExistingCallerAgentAPI.as_view()),
     url(r'^current_user/achievements$', views.api_user_achievements.as_view()),
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += [
+    url(r'^test_websocket$', views.api_test_socket.as_view()),
+    url(r'^simulate_call$', views.api_test_simulatecall.as_view()),
+    url(r'^simulate_achievement$', views.api_test_simulateachievement.as_view())
+    ]
